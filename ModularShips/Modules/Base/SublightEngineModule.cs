ddc205d2aeb4c20180e9e2a2641e1b5b00408de6
Ingannatore <matteo.guindani@gmail.com>
2020-05-16 21:13:20 +0200

    Add angular acceleration

--- ModularShips/Modules/Base/SublightEngineModule.cs
+++ ModularShips/Modules/Base/SublightEngineModule.cs
@@ -4,15 +4,17 @@ namespace ModularShips.Modules.Base
 {
     public abstract class SublightEngineModule : Module
     {
-        public int DeltaV { get; }
+        public int DeltaSpeed { get; }
+        public int DeltaRotation { get; }
         public Speed Speed { get; }
 
-        protected SublightEngineModule(string name, Size size, int deltaV, Speed speed) : base(
+        protected SublightEngineModule(string name, Size size, int deltaSpeed, int deltaRotation, int maxSpeed) : base(
             name, size, ModuleCategory.SublightEngine
         )
         {
-            DeltaV = deltaV;
-            Speed = speed;
+            DeltaSpeed = deltaSpeed;
+            DeltaRotation = deltaRotation;
+            Speed = new Speed(maxSpeed);
         }
     }
 }
